007a38463d327220045e7a33fef8ae8d3599bdb7
jerryhart19 <53445302+jerryhart19@users.noreply.github.com>
2019-07-29 23:06:02 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+header("Location:https://www.ebay.com/itm/1-Pcs-Car-Scratch-Repair-Wax-100ml-Remove-Scratches-Paint-Body-Care-Non-toxic/132879091352?epid=23030073959&hash=item1ef0360698:g:B9EAAOSwBqZcBlwf")
+?>
